8b5b5d6206691fddf133c43adc9ce631f0c94521
Isaac-DeFrain <isaacdefrain@gmail.com>
2023-03-18 07:07:15 +0000

    Apply ledger diff and debug

--- tree/src/ledger.rs
+++ tree/src/ledger.rs
@@ -1,25 +1,66 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::Debug};
 
 #[derive(Debug, Clone)]
 struct Account {
     pk: String,
+    delegate: String,
+    balance: u64,
     stake: u64,
     delegations: u64,
 }
 
-#[derive(Debug)]
+#[derive(Clone)]
 pub struct Ledger(HashMap<String, Account>);
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct LedgerDiff(HashMap<(String, String), u64>);
 
 impl Account {
-    pub fn new(pk: String, stake: u64, delegations: u64) -> Self {
+    pub fn new(pk: String, balance: u64, delegations: u64) -> Self {
         Self {
+            delegate: pk.clone(),
             pk,
-            stake,
+            balance,
+            stake: balance,
             delegations,
         }
+    }
+}
+
+impl Ledger {
+    pub fn apply(&mut self, diff: LedgerDiff) {
+        for ((from, to), amount) in diff.0 {
+            match self.0.get_mut(&from) {
+                None => {
+                    continue;
+                }
+                Some(from_acct) => {
+                    if from_acct.balance < amount {
+                        continue;
+                    } else {
+                        from_acct.stake -= amount;
+                        match self.0.get_mut(&to) {
+                            None => {
+                                self.0.insert(to.clone(), Account::new(to, amount, 0));
+                            }
+                            Some(to_acct) => {
+                                to_acct.balance += amount;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl Debug for Ledger {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let res = writeln!(f, "=== Ledger ===");
+        for (pk, acct) in &self.0 {
+            writeln!(f, "pk:   {:?},\nacct: {:?}\n", pk, acct).unwrap();
+        }
+        res
     }
 }
 
@@ -41,6 +82,16 @@ impl LedgerDiff {
     }
 }
 
+impl Debug for LedgerDiff {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[ ").unwrap();
+        for ((from, to), amount) in self.0.clone() {
+            write!(f, "{:?} ", (from, to, amount)).unwrap()
+        }
+        write!(f, "]")
+    }
+}
+
 #[test]
 pub fn ledger_example() {
     let mut ledger = Ledger(HashMap::new());
@@ -59,6 +110,11 @@ pub fn ledger_example() {
     ledger
         .0
         .insert(bp2.clone(), Account::new(bp2.clone(), 87, 10));
+
+    println!("{:?}", ledger);
+
+    let diff = LedgerDiff::from(&[("a", "b", 20), ("b", "c", 12)]);
+    ledger.apply(diff);
 
     println!("{:?}", ledger);
     // assert!(false);
