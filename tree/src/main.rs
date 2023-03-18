64abf5464f998690dac8629eef84d3a7cc8a5a8d
Isaac-DeFrain <isaacdefrain@gmail.com>
2023-03-18 05:57:11 +0000

    Import modules in main

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -1,7 +1,12 @@
+mod ledger;
 mod merge;
+mod weighted_forest;
 mod weighted_tree;
 
 fn main() {
-    merge::merge_example();
-    weighted_tree::add_weighted_block_example();
+    println!("Main");
+    // ledger::ledger_example();
+    // merge::merge_example();
+    // weighted_tree::add_weighted_block_example();
+    // weighted_forest::forest_example;
 }
