5b8a669b6c853626a44f09d73fbf30edd07590e8
Isaac-DeFrain <quantifier-tech@protonmail.com>
2023-06-20 20:22:01 +0000

    (gsutil) Add fs dep

--- gsutil/src/main.rs
+++ gsutil/src/main.rs
@@ -1,6 +1,7 @@
+use fs::{check_dir, check_file};
 use clap::Parser;
 use std::{
-    fs::{create_dir_all, File, OpenOptions},
+    fs::OpenOptions,
     io::prelude::*,
     path::PathBuf,
     process::{Command, Stdio},
@@ -60,22 +61,5 @@ fn main() -> anyhow::Result<()> {
     match gsutil_cmd.wait() {
         Ok(_) => Ok(()),
         Err(e) => Err(anyhow::Error::from(e)),
-    }
-}
-
-fn check_file(file: &PathBuf) {
-    if file.exists() {
-        assert!(file.is_file(), "{} must be a file!", file.display());
-    } else {
-        create_dir_all(file.parent().unwrap()).unwrap();
-        File::create(file).unwrap();
-    }
-}
-
-fn check_dir(dir: &PathBuf) {
-    if dir.exists() {
-        assert!(dir.is_dir(), "{} must be a dir!", dir.display());
-    } else {
-        create_dir_all(dir).unwrap()
     }
 }
